2ef7cef8750c93cc5a476d361d15e580e1217315
91657 <91657@TIC-USER17>
2024-04-25 01:16:12 -0500

    Agregar archivos de proyecto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace aruizT1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void btnCalcular_Clicked(object sender, EventArgs e)
+        {
+            String nombre =txtNombre.Text;
+            string apellido = txtApellido.Text;
+            int edad = Convert.ToInt32(txtEdad.Text);
+            double salario= Convert.ToDouble(txtSalario.Text);
+            double aporte = (salario * 9.45) / 100;
+
+            DisplayAlert("Bienvenido",nombre + apellido +"\nTienes "+edad+" años"+"\nTu aporte mensual es "+aporte,"Cerrar");
+        }
+    }
+
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace aruizT1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
